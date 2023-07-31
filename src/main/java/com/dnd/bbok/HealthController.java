2c6941cdec5ace4d8f5177fed2b295b6b9714818
D0ri123 <lsh81550@gmail.com>
2023-07-31 11:18:24 +0900

    feat: 응답형식 예시 보여주는 controller 생성

--- src/main/java/com/dnd/bbok/HealthController.java
+++ src/main/java/com/dnd/bbok/HealthController.java
@@ -1,7 +1,10 @@
 package com.dnd.bbok;
 
+import com.dnd.bbok.global.response.DataResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +16,16 @@ public class HealthController {
   @GetMapping("/health")
   public String healthCheck() {
     return "hello";
+  }
+
+  /**
+   * Data reponse에 감싸져있는 HTTP status는 응답 body에 노출됨
+   */
+  @ApiOperation(value = "응답형식 예시")
+  @GetMapping("/response")
+  public ResponseEntity<DataResponse<String>> responseCheck() {
+    return new ResponseEntity<>(DataResponse.of(
+        HttpStatus.OK, "응답 형식 지정 성공", "ok"), HttpStatus.OK);
   }
 
 }
