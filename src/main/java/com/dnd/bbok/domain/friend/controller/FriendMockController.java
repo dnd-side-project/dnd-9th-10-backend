104113eb2b2dfd731c1d0c23c2ec477f8fbd6634
D0ri123 <lsh81550@gmail.com>
2023-07-31 17:21:32 +0900

    feat: 친구 등록 mock 컨트롤러 생성

--- src/main/java/com/dnd/bbok/domain/friend/controller/FriendMockController.java
+++ src/main/java/com/dnd/bbok/domain/friend/controller/FriendMockController.java
@@ -1,13 +1,17 @@
 package com.dnd.bbok.domain.friend.controller;
 
+import com.dnd.bbok.domain.friend.dto.request.FriendRequestDto;
 import com.dnd.bbok.domain.friend.dto.response.BbokCharactersDto;
 import com.dnd.bbok.domain.friend.dto.response.FriendsDto;
 import com.dnd.bbok.global.response.DataResponse;
+import com.dnd.bbok.global.response.MessageResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +34,14 @@ public class FriendMockController {
     FriendsDto friends = new FriendsDto();
     return new ResponseEntity<>(
         DataResponse.of(HttpStatus.OK, "친구 목록 조회 성공", friends), HttpStatus.OK);
+  }
+
+  @ApiOperation(value = "친구 등록")
+  @PostMapping("/friend")
+  public ResponseEntity<MessageResponse> createFriend(
+      @RequestBody FriendRequestDto requestFriend) {
+    return new ResponseEntity<>(
+        MessageResponse.of(HttpStatus.CREATED, "친구 등록 성공"), HttpStatus.CREATED);
   }
 
 }
