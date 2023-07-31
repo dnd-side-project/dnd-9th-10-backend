83f9ba3afaeca530d300ac337aff245eb5549773
D0ri123 <lsh81550@gmail.com>
2023-07-31 11:17:20 +0900

    feat: 예외 처리 관련 클래스 생성

---
+++ src/main/java/com/dnd/bbok/global/exception/BusinessException.java
@@ -0,0 +1,22 @@
+package com.dnd.bbok.global.exception;
+
+import lombok.Getter;
+
+/**
+ * 직접 정의한 에러코드와 메시지를 보내기 위해서 해당 클래스를 상속받아야 함
+ */
+@Getter
+public abstract class BusinessException extends RuntimeException {
+
+  private final ErrorCode errorCode;
+
+  protected BusinessException(ErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.errorCode = errorCode;
+  }
+
+  public ErrorCode getErrorCode(){
+    return errorCode;
+  }
+
+}
