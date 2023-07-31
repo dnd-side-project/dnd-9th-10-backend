b111e4119e2abb418c618cbeb01f1e6026a4377b
D0ri123 <lsh81550@gmail.com>
2023-07-31 16:44:05 +0900

    feat: 캐릭터 관련 정보 제공 dto 생성

---
+++ src/main/java/com/dnd/bbok/domain/friend/dto/response/BbokCharacterDto.java
@@ -0,0 +1,26 @@
+package com.dnd.bbok.domain.friend.dto.response;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+
+/**
+ * 캐릭터 1개에 대한 response Dto
+ */
+@Getter
+public class BbokCharacterDto {
+
+  @ApiModelProperty(value = "캐릭터 아이콘 Url")
+  private final String iconUrl;
+
+  @ApiModelProperty(value = "캐릭터 이름")
+  private final String name;
+
+  /**
+   * mock 제공을 위한 기본생성
+   */
+  public BbokCharacterDto() {
+    this.iconUrl = "선인장 아이콘 url";
+    this.name = "인장이";
+  }
+
+}
