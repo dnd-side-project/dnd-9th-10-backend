a1941d662b35a73c5e2708a0accaea1abbe4647f
D0ri123 <lsh81550@gmail.com>
2023-08-01 13:24:12 +0900

    feat: 북마크 등록 mock 컨트롤러 구현

--- src/main/java/com/dnd/bbok/domain/saying/controller/SayingMockController.java
+++ src/main/java/com/dnd/bbok/domain/saying/controller/SayingMockController.java
@@ -1,12 +1,16 @@
 package com.dnd.bbok.domain.saying.controller;
 
+import com.dnd.bbok.domain.saying.dto.request.BookmarkRequestDto;
 import com.dnd.bbok.domain.saying.dto.response.BookmarkInfoDto;
 import com.dnd.bbok.global.response.DataResponse;
+import com.dnd.bbok.global.response.MessageResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +27,13 @@ public class SayingMockController {
         DataResponse.of(HttpStatus.OK, "북마크 목록 조회 성공", bookmarks), HttpStatus.OK);
   }
 
+  @ApiOperation(value = "북마크 추가")
+  @PostMapping("")
+  public ResponseEntity<MessageResponse> createBookmark(
+      @RequestBody BookmarkRequestDto bookmarkRequest
+  ) {
+    return new ResponseEntity<>(
+        MessageResponse.of(HttpStatus.CREATED, "북마크 등록 성공"), HttpStatus.CREATED);
+  }
 
 }
---
+++ src/main/java/com/dnd/bbok/domain/saying/dto/request/BookmarkRequestDto.java
@@ -0,0 +1,12 @@
+package com.dnd.bbok.domain.saying.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class BookmarkRequestDto {
+
+  private Long id;
+
+}
