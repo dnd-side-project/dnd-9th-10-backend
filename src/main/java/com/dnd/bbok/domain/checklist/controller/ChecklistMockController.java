d314faa8a5330f58088c91b1df6fa3ca3746de95
D0ri123 <lsh81550@gmail.com>
2023-08-01 12:40:21 +0900

    feat: 사용자 정의 체크리스트 관련 컨트롤러 구현

--- src/main/java/com/dnd/bbok/domain/checklist/controller/ChecklistMockController.java
+++ src/main/java/com/dnd/bbok/domain/checklist/controller/ChecklistMockController.java
@@ -1,12 +1,16 @@
 package com.dnd.bbok.domain.checklist.controller;
 
+import com.dnd.bbok.domain.checklist.dto.request.MemberChecklistRequestDto;
 import com.dnd.bbok.domain.checklist.dto.response.BasicChecklistDto;
 import com.dnd.bbok.global.response.DataResponse;
+import com.dnd.bbok.global.response.MessageResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +25,15 @@ public class ChecklistMockController {
     BasicChecklistDto basicChecklist = new BasicChecklistDto();
     return new ResponseEntity<>(
         DataResponse.of(HttpStatus.OK, "기본 체크리스트 제공 성공", basicChecklist), HttpStatus.OK);
+  }
+
+  @ApiOperation(value = "사용자 정의 체크리스트 추가")
+  @PostMapping("")
+  public ResponseEntity<MessageResponse> createChecklist(
+      @RequestBody MemberChecklistRequestDto memberChecklist
+  ) {
+    return new ResponseEntity<>(
+        MessageResponse.of(HttpStatus.CREATED, "체크리스트 추가 성공"), HttpStatus.CREATED);
   }
 
 
