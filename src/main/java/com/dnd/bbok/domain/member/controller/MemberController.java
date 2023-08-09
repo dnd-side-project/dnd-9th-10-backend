75b2ed94b726048bf9e3f24ff78dec185a69cb8c
D0ri123 <lsh81550@gmail.com>
2023-08-09 15:58:46 +0900

    chore: swagger 관련 설정 반영

--- src/main/java/com/dnd/bbok/domain/member/controller/MemberController.java
+++ src/main/java/com/dnd/bbok/domain/member/controller/MemberController.java
@@ -16,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,7 +36,9 @@ public class MemberController {
   /**
    * 게스트 로그인 관련 컨트롤러
    */
-  @ApiOperation(value = "게스트 회원가입")
+  @ApiOperation(
+      value = "게스트 회원가입",
+      notes = "요청 보내면 바로 게스트가 생성되고, accessToken이 발급됩니다.")
   @PostMapping("/api/v1/guest/signup")
   public ResponseEntity<DataResponse<MemberSimpleInfoResponse>> guestLogin() {
 
@@ -49,7 +52,10 @@ public class MemberController {
   /**
    * 유저가 자기 자신의 정보에 대해 알 수 있다.
    */
-  @ApiOperation(value = "내 정보 조회")
+  @ApiOperation(
+      value = "내 정보 조회",
+      notes = "마이 페이지에서 사용자의 정보를 볼 수 있습니다.")
+  @PreAuthorize("isAuthenticated()")
   @GetMapping("/api/v1/member")
   public ResponseEntity<DataResponse<MemberInfoResponseDto>> getMember(
       @AuthenticationPrincipal SessionUser sessionUser
@@ -62,8 +68,10 @@ public class MemberController {
   /**
    * 로그인 요청을 통해 인가코드를 redirect url로 발급 가능
    */
-  @ApiOperation(value = "인가 코드 발급")
-  @GetMapping("/api/v1/login/kakao")
+  @ApiOperation(
+      value = "인가 코드 발급",
+      notes = "해당 url을 통해 로그인 화면으로 넘어간 후, 사용자가 정보를 입력하면 redirect url에서 코드를 발급할 수 있습니다.")
+  @GetMapping("/api/v1/kakao/login")
   public ResponseEntity<HttpHeaders> getKakaoAuthCode()  {
     HttpHeaders httpHeaders = kakaoFeignService.kakaoLogin();
     return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
@@ -72,7 +80,9 @@ public class MemberController {
   /**
    * 인가코드를 통해 accessToken 과 유저 정보를 가져온다.
    */
-  @ApiOperation(value = "카카오 계정 회원가입")
+  @ApiOperation(
+      value = "카카오 계정 회원가입",
+      notes = "인가 코드를 입력하고 요청보내면, 사용자의 정보를 저장한 후 사용자의 Id를 확인할 수 있습니다.")
   @PostMapping("/api/v1/kakao/signup")
   public ResponseEntity<DataResponse<MemberSimpleInfoResponse>> kakaoLogin(@RequestParam("code") String code) {
 
