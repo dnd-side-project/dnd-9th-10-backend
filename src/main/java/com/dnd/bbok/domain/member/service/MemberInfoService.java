740a231dc9696ca33368529b546044111d172944
D0ri123 <lsh81550@gmail.com>
2023-08-11 03:42:38 +0900

    refactor: 예외 처리 로직 제거

--- src/main/java/com/dnd/bbok/domain/member/service/MemberInfoService.java
+++ src/main/java/com/dnd/bbok/domain/member/service/MemberInfoService.java
@@ -1,10 +1,7 @@
 package com.dnd.bbok.domain.member.service;
 
-import static com.dnd.bbok.global.exception.ErrorCode.MEMBER_NOT_FOUND;
-
 import com.dnd.bbok.domain.member.dto.response.MemberInfoResponseDto;
 import com.dnd.bbok.domain.member.entity.Member;
-import com.dnd.bbok.domain.member.exception.MemberNotFoundException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,8 +13,7 @@ public class MemberInfoService {
   private final MemberService memberService;
 
   public MemberInfoResponseDto getMember(UUID memberId) {
-    Member member = memberService.getMemberById(memberId)
-        .orElseThrow(() -> new MemberNotFoundException(MEMBER_NOT_FOUND));
+    Member member = memberService.getMemberById(memberId);
     return new MemberInfoResponseDto(member);
   }
 }
