600eba052bf06d85e8902377144e2307ab76f838
Walkers15 <qortmd7777@gmail.com>
2023-08-10 23:19:59 +0900

    etc: 불필요 어노테이션 제거, security config 변경

--- src/main/java/com/dnd/bbok/domain/checklist/repository/BasicChecklistRepository.java
+++ src/main/java/com/dnd/bbok/domain/checklist/repository/BasicChecklistRepository.java
@@ -3,8 +3,6 @@ package com.dnd.bbok.domain.checklist.repository;
 
 import com.dnd.bbok.domain.checklist.entity.BasicChecklist;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BasicChecklistRepository extends JpaRepository<BasicChecklist, Long> {
 }
--- src/main/java/com/dnd/bbok/global/security/SecurityConfig.java
+++ src/main/java/com/dnd/bbok/global/security/SecurityConfig.java
@@ -30,8 +30,7 @@ public class SecurityConfig {
 
     http//HTTP 헤더에 사용자의 이름과 암호 포함을 비활성화
         .authorizeRequests()
-        .antMatchers(HttpMethod.GET, "/test", "/api/v1/member").authenticated() //해당 요청은 인증이 필요하다.
-        .antMatchers(HttpMethod.GET, "/api/v1/checklist").authenticated() //해당 요청은 인증이 필요하다.
+        .antMatchers(HttpMethod.GET, "/test", "/api/v1/member", "/api/v1/checklist").authenticated() //해당 요청은 인증이 필요하다.
         .antMatchers("/**").permitAll() //해당 요청은 누구나 다 들어올 수 있다.
         .and()
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
