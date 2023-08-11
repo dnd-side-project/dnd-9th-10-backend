d4547ce1e5c516f4d3375a3a5d59e299faa6fbb6
D0ri123 <lsh81550@gmail.com>
2023-08-12 01:47:03 +0900

    setting: aws-sdk, s3 관련 설정

--- src/main/java/com/dnd/bbok/ServerApplication.java
+++ src/main/java/com/dnd/bbok/ServerApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerApplication {
-
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
