47ad565c6a00d447c7f16361b0b43cdeff16b8ba
D0ri123 <lsh81550@gmail.com>
2023-08-12 17:24:00 +0900

    chore: enable jpa auditing을 설정하여 created_at, modified_at 반영

--- src/main/java/com/dnd/bbok/ServerApplication.java
+++ src/main/java/com/dnd/bbok/ServerApplication.java
@@ -2,7 +2,9 @@ package com.dnd.bbok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ServerApplication {
 	static {
