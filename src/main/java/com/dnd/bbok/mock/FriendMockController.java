dca6b2afa702a769f8cf5a88e0b98995934cf5f5
D0ri123 <lsh81550@gmail.com>
2023-08-14 13:41:47 +0900

    fix: github action build 오류 해결

--- src/main/java/com/dnd/bbok/mock/FriendMockController.java
+++ src/main/java/com/dnd/bbok/mock/FriendMockController.java
@@ -1,14 +1,11 @@
 package com.dnd.bbok.mock;
 
 import com.dnd.bbok.domain.checklist.dto.request.ChecklistInfoRequestDto;
-import com.dnd.bbok.domain.checklist.dto.response.MyChecklistDto;
-import com.dnd.bbok.global.response.DataResponse;
 import com.dnd.bbok.global.response.MessageResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,14 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 @Api(tags = "Mock 친구 관련 컨트롤러")
 public class FriendMockController {
-
-  @ApiOperation(value = "나만의 기준 조회")
-  @GetMapping("/friend/checklist")
-  public ResponseEntity<DataResponse<MyChecklistDto>> getChecklist() {
-    MyChecklistDto myChecklist = new MyChecklistDto();
-    return new ResponseEntity<>(
-        DataResponse.of(HttpStatus.OK, "기준 조회 성공", myChecklist), HttpStatus.OK);
-  }
 
   @ApiOperation(value = "나만의 기준 수정")
   @PatchMapping("/friend/checklist")
