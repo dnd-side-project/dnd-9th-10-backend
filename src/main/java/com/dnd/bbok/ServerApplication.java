14ec15773100362987057cab7c4656146320a717
D0ri123 <lsh81550@gmail.com>
2023-08-20 17:33:04 +0900

    fix: allowed method 추가 설정

--- src/main/java/com/dnd/bbok/ServerApplication.java
+++ src/main/java/com/dnd/bbok/ServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -26,7 +27,12 @@ public class ServerApplication {
 						.allowedOrigins(
 								"http://localhost:3000",
 								"https://bbok.vercel.app",
-								"https://bbok.kro.kr");
+								"https://bbok.kro.kr")
+						.allowedMethods(
+								HttpMethod.GET.name(),
+								HttpMethod.PATCH.name(),
+								HttpMethod.DELETE.name(),
+								HttpMethod.POST.name());
 			}
 		};
 	}
