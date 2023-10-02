750bc810365b6298ccc58b2d4e1790e0570acad4
Seolhui Lee <94590894+D0ri123@users.noreply.github.com>
2023-10-02 18:24:54 +0900

    fix: 배포설정, swagger 설정 수정 (#91)

--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:11
 ARG JAR_FILE=build/libs/*.jar
 COPY ${JAR_FILE} server.jar
-ENTRYPOINT ["java","-jar","/server.jar"]
+ENTRYPOINT ["java","-jar","-Dspring.profiles.active=prod","/server.jar"]
---
+++ src/main/java/com/dnd/bbok/infra/swagger/Workaround.java
@@ -0,0 +1,34 @@
+package com.dnd.bbok.infra.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.Arrays;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+import springfox.documentation.oas.web.OpenApiTransformationContext;
+import springfox.documentation.oas.web.WebMvcOpenApiTransformationFilter;
+import springfox.documentation.spi.DocumentationType;
+
+@Component
+public class Workaround implements WebMvcOpenApiTransformationFilter {
+  @Override
+  public OpenAPI transform(OpenApiTransformationContext<HttpServletRequest> context) {
+    OpenAPI openApi = context.getSpecification();
+
+    Server localServer = new Server();
+    localServer.setDescription("usages for local");
+    localServer.setUrl("http://localhost:8080");
+
+    Server prodServer = new Server();
+    prodServer.setDescription("usages for real");
+    prodServer.setUrl("https://bbok.o-r.kr");
+
+    openApi.setServers(Arrays.asList(localServer, prodServer));
+    return openApi;
+  }
+
+  @Override
+  public boolean supports(DocumentationType docType) {
+    return docType.equals(DocumentationType.OAS_30);
+  }
+}
